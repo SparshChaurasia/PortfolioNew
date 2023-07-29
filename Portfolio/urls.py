8e859cbae7669b453ef1934c5a63331d2143bb87
SparshChaurasia <sparshchaurasia0021@gmail.com>
2023-07-29 20:33:06 +0530

    added seprate projects page

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.home import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.home.urls"))
+    path("", include("apps.home.urls")),
+    path("projects/<str:project>", views.projects)
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -29,3 +29,15 @@ def submit_contact_form(request):
     form.save()
     messages.success(request, "Your form was successfully submitted")
     return redirect("/#contact")
+
+def projects(request, project):
+    templates_list = {
+        "chat-app": "chat-app.html", 
+        "discord-bot": "discord-bot.html", 
+        "rfid-attendance": "rfid-attendance.html",
+        "voting-system": "voting-system.html"
+    }
+
+    template = templates_list.get(project)
+
+    return render(request, f"projects/{template}")
